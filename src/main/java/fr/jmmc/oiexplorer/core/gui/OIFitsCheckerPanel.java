df0e9384bdd05dfb15dd2d71f3201e37986fda2e
bourgesl <bourges.laurent@gmail.com>
2022-10-19 19:29:55 +0200

    use pref + give link to jmmc rules

--- src/main/java/fr/jmmc/oiexplorer/core/gui/OIFitsCheckerPanel.java
+++ src/main/java/fr/jmmc/oiexplorer/core/gui/OIFitsCheckerPanel.java
@@ -4,6 +4,7 @@
 package fr.jmmc.oiexplorer.core.gui;
 
 import fr.jmmc.jmcs.gui.component.ResizableTextViewFactory;
+import fr.jmmc.oiexplorer.core.Preferences;
 import fr.jmmc.oitools.model.OIFitsChecker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,18 +18,28 @@ public final class OIFitsCheckerPanel {
     /** Class logger */
     private static final Logger logger = LoggerFactory.getLogger(OIFitsChecker.class.getName());
 
-    public static void displayReport(final OIFitsChecker checker) {
+    public static void displayReport(final OIFitsChecker checker, final Preferences preferences) {
         if (checker != null) {
+            // get validation messages in compact mode:
+            final String compactReport = checker.getCheckReport(true);
             // log validation messages anyway:
-            logger.info("validation results:\n{}", checker.getCheckReport()); // full mode
+            logger.info("validation results:\n{}", compactReport);
 
             // TODO: use a preference to show or hide the validation report:
-            if (true && !checker.isEmpty()) {
-                // display validation messages in compact mode:
-                final String compactReport = checker.getCheckReport(true);
+            if (!checker.isEmpty()) {
+                // return false if the preference value is missing:
+                final boolean dontShow = (preferences != null) ? preferences.getPreferenceAsBoolean(Preferences.VALIDATOR_DONT_SHOW_REPORT, true) : false;
 
-                // Create model dialog:
-                ResizableTextViewFactory.createTextWindow(compactReport, "OIFits validator", true);
+                if (!dontShow) {
+                    // display validation messages:
+                    final String text = "JMMC OIFITS Validator rules defined at:\n"
+                            + "https://jmmc-opendev.github.io/oitools/rules/DataModelV2_output.html"
+                            + "\n\n" + compactReport;
+
+                    // TODO: use HTML for color codes ?
+                    // Create model dialog:
+                    ResizableTextViewFactory.createTextWindow(text, "OIFits validator", true);
+                }
             }
         }
     }
